2bab1ac312c1f4f32564c5b12f838c539cfb43fb
Muhaddis <muhaddisshah@gmail.com>
2021-09-01 21:26:38 +0500

    :sparkles: get payload and alert on the issue if it does not exist

--- index.js
+++ index.js
@@ -2,13 +2,46 @@ const core = require("@actions/core");
 const github = require("@actions/github");
 
 try {
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput("who-to-greet");
-  console.log(`Hello ${nameToGreet}!`);
+  const github_token = core.getInput("GITHUB_TOKEN");
+  const Labels = core.getInput("labels");
+  const issueLabels = github.context.payload.labels;
+
+  console.log(issueLabels);
+  console.log(Labels);
+
+  if (Labels) {
+    console.log(`Hello ${Labels}!`);
+  }
   const time = new Date().toTimeString();
   core.setOutput("time", time);
   // Get the JSON webhook payload for the event that triggered the workflow
+
+  for (let index = 0; index < Labels.length; index++) {
+    if (issueLabels.includes(Labels[index])) {
+      console.log(Labels[index], " issue exists");
+    } else {
+      const message =
+        ("The following labels " + Labels[index],
+        " does not exist on the issue. Please add these labels to avoid any inconvenience");
+
+      const octokit = new github.GitHub(github_token);
+      octokit.issues.createComment({
+        issue_number: issue_number,
+        body: message,
+      });
+
+      octokit.issues.createComment({
+        issue_number: context.issue.number,
+        owner: context.repo.owner,
+        repo: github.context.repo.repo,
+        body: message,
+      });
+      // Let them know
+    }
+  }
+
   const payload = JSON.stringify(github.context.payload, undefined, 2);
+
   console.log(`The event payload: ${payload}`);
 } catch (error) {
   core.setFailed(error.message);
