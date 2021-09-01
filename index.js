f5b2694f06d166583a1243e80c86f753be1a7f39
Muhaddis <muhaddisshah@gmail.com>
2021-09-02 00:15:00 +0500

    :rocket: move function inside the await call

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ try {
   const github_token = core.getInput("GITHUB_TOKEN");
   const Labels = core.getInput("labels").split(",");
   const labelsObject = github.context.payload.issue.labels;
+  const octokit = github.getOctokit(github_token);
 
   let issueLabels = [];
 
@@ -22,10 +23,10 @@ try {
         ("The following labels " + Labels[index],
         " does not exist on the issue. Please add these labels to avoid any inconvenience");
 
-      const octokit = github.getOctokit(github_token);
-
-      octokit.rest.issues.createComment({
-        issue_number: github.context.payload.issue.number,
+      await octokit.issues.createComment({
+        issue_number: github.context.issue.number,
+        owner: github.context.repo.owner,
+        repo: github.context.repo.repo,
         body: message,
       });
 
