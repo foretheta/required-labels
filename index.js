53cf561dd06888b7d7f206ee7e9a76dca3367e4c
Muhaddis <muhaddisshah@gmail.com>
2021-09-06 18:47:20 +0500

    remove second condition

--- index.js
+++ index.js
@@ -23,20 +23,20 @@ try {
   );
 
   if (isAtleastOneValidLabelAdded === false) {
-    if (missingLabels > 0) {
-      const missingLabelsString = missingLabels.join(", ");
-      const message =
-        "The following label **" +
-        missingLabelsString +
-        "** does not exist on the issue. Please add these labels to avoid any inconvenience in future.";
+    // if (missingLabels > 0) {
+    const missingLabelsString = missingLabels.join(", ");
+    const message =
+      "The following label **" +
+      missingLabelsString +
+      "** does not exist on the issue. Please add these labels to avoid any inconvenience in future.";
 
-      octokit.rest.issues.createComment({
-        issue_number: github.context.issue.number,
-        owner: github.context.repo.owner,
-        repo: github.context.repo.repo,
-        body: message,
-      });
-    }
+    octokit.rest.issues.createComment({
+      issue_number: github.context.issue.number,
+      owner: github.context.repo.owner,
+      repo: github.context.repo.repo,
+      body: message,
+    });
+    // }
   }
 } catch (error) {
   core.setFailed(error.message);
