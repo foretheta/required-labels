699223926933040b80a4610bd339ec535771fc1e
Muhaddis <muhaddisshah@gmail.com>
2021-09-06 18:28:06 +0500

    pass condition on one valid label

--- dist/index.js
+++ dist/index.js
@@ -6335,22 +6335,25 @@ try {
     return !labelsInIssue.includes(requiredLabel);
   });
 
-  if (
-    !labelsInIssue.some((label) => requiredLabels.indexOf(label)) > 0 &&
-    missingLabels > 0
-  ) {
-    const missingLabelsString = missingLabels.join(", ");
-    const message =
-      "The following label **" +
-      missingLabelsString +
-      "** does not exist on the issue. Please add these labels to avoid any inconvenience in future.";
+  const isAtleastOneValidLabelAdded = requiredLabels.some(
+    (label) => labelsInIssue.indexOf(label) > 0
+  );
 
-    octokit.rest.issues.createComment({
-      issue_number: github.context.issue.number,
-      owner: github.context.repo.owner,
-      repo: github.context.repo.repo,
-      body: message,
-    });
+  if (isAtleastOneValidLabelAdded === true) {
+    if (missingLabels > 0) {
+      const missingLabelsString = missingLabels.join(", ");
+      const message =
+        "The following label **" +
+        missingLabelsString +
+        "** does not exist on the issue. Please add these labels to avoid any inconvenience in future.";
+
+      octokit.rest.issues.createComment({
+        issue_number: github.context.issue.number,
+        owner: github.context.repo.owner,
+        repo: github.context.repo.repo,
+        body: message,
+      });
+    }
   }
 } catch (error) {
   core.setFailed(error.message);
