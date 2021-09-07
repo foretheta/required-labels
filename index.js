b0285f4504ec86e7dc8f2de4b724c02389ae78c6
Muhaddis <muhaddisshah@gmail.com>
2021-09-07 14:38:41 +0500

    update conditional statements

--- index.js
+++ index.js
@@ -13,27 +13,29 @@ try {
     return !labelsInIssue.includes(requiredLabel);
   });
 
-  const isAtleastOneValidLabelAdded = () => {
-    requiredLabels.length === 0 ? false : validIssues();
-  };
-  if (
-    labelsInIssue.length !== 0 &&
-    labelsInIssue.some((label) => requiredLabels.indexOf(label) > 0) !== true
-  ) {
-    // if (missingLabels > 0) {
-    const missingLabelsString = missingLabels.join(", ");
-    const message =
-      "The following label **" +
-      missingLabelsString +
-      "** does not exist on the issue. Please add these labels to avoid any inconvenience in future.";
+  const missingLabelsString = missingLabels.join(", ");
+  const message =
+    "The following label **" +
+    missingLabelsString +
+    "** does not exist on the issue. Please add these labels to avoid any inconvenience in future.";
 
+  if (labelsInIssue.length === 0) {
     octokit.rest.issues.createComment({
       issue_number: github.context.issue.number,
       owner: github.context.repo.owner,
       repo: github.context.repo.repo,
       body: message,
     });
-    // }
+  } else if (
+    missingLabels.length !== requiredLabels.length &&
+    missingLabels.length > 1
+  ) {
+    octokit.rest.issues.createComment({
+      issue_number: github.context.issue.number,
+      owner: github.context.repo.owner,
+      repo: github.context.repo.repo,
+      body: message,
+    });
   }
 } catch (error) {
   core.setFailed(error.message);
